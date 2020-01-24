263cebdc689ee848ccaa6511fa528fcb86398524
Frederik Baun Hansen <frederik.baun@dentsuaegis.com>
2020-01-24 19:06:49 +0100

    feat: Add service collection DI support

---
+++ Adobe/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Adobe.Extensions
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Services.Authorization;
+    using Services.Endpoints;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAdobeServices(this IServiceCollection services)
+        {
+            services.AddSingleton<AdobeClientStore>();
+
+            services.AddTransient<AdobeAuthorizationService>();
+
+            services.AddTransient<AdobeAuthorizationService>();
+            services.AddTransient<DimensionService>();
+            services.AddTransient<MetricService>();
+            services.AddTransient<ReportService>();
+            services.AddTransient<SegmentService>();
+            services.AddTransient<UserService>();
+        }
+    }
+}
