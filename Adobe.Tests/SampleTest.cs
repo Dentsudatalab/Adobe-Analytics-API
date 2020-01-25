cc9c6dcdf6e548c0c58c2d27dbb6e130557cd734
Frederik Baun Hansen <frederik.baun@dentsuaegis.com>
2020-01-25 23:55:23 +0100

    feat: Sample test class

---
+++ Adobe.Tests/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Adobe.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void SampleTest_ShouldPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
