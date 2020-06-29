c574286d9be553fa8be127ffd891cf20aac219cd
Kasper Hesthaven <kasper.hesthaven@dentsuaegis.com>
2020-06-29 07:51:01 +0200

    style: Applied R# Rider linting

--- Adobe.Tests/Utility/Auth/JwtCreatorTests.cs
+++ Adobe.Tests/Utility/Auth/JwtCreatorTests.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.IO;
+
     using Adobe.Utility.Auth;
+
     using NUnit.Framework;
 
     [TestFixture]
@@ -35,7 +37,8 @@
             var jwt = JwtCreator.CreateJwt(authValues, exp);
 
             // Assert
-            const string expectedJwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzc4Mzc0MDAsImlzcyI6Im9yZ0lkIiwic3ViIjoidGVjaElkIiwiaHR0cHM6Ly9pbXMtbmExLmFkb2JlbG9naW4uY29tL3MvZW50X2FuYWx5dGljc19idWxrX2luZ2VzdF9zZGsiOnRydWUsImF1ZCI6Imh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9jL2NsaWVudElkIn0.t-9dajjHsuxCNghRPV0h82r7JEpTX5uUfUi_UGbiqOlJlBWBeXyzxaHIqWzWOLNKaJeK6Kzmv72zc8KrPZzyqtjvd5Q98A4CwFqxyNMatS-7aCWPX46bP4PaguWHSwt-th9SkB6vBIvwR7cs0wmgJJdrDn-Rf32B0m_xh3fx4aV7w3BDJqCujbrx98VmP6LMRyVSvwikjHx9vQ4YExjDnhNazeW0_P7c97dBxXInFHg1_sF0cAjWoDKSSV-PF4HSexLnLaPAx2bMj1ZGJHAf6JXICqVqRzxXkEsoWuirHxdJY5NSlQfkw4jFXl1CPrPI9doUlZ2X7hW_f6_Afu6M8g";
+            const string expectedJwt =
+                "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzc4Mzc0MDAsImlzcyI6Im9yZ0lkIiwic3ViIjoidGVjaElkIiwiaHR0cHM6Ly9pbXMtbmExLmFkb2JlbG9naW4uY29tL3MvZW50X2FuYWx5dGljc19idWxrX2luZ2VzdF9zZGsiOnRydWUsImF1ZCI6Imh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9jL2NsaWVudElkIn0.t-9dajjHsuxCNghRPV0h82r7JEpTX5uUfUi_UGbiqOlJlBWBeXyzxaHIqWzWOLNKaJeK6Kzmv72zc8KrPZzyqtjvd5Q98A4CwFqxyNMatS-7aCWPX46bP4PaguWHSwt-th9SkB6vBIvwR7cs0wmgJJdrDn-Rf32B0m_xh3fx4aV7w3BDJqCujbrx98VmP6LMRyVSvwikjHx9vQ4YExjDnhNazeW0_P7c97dBxXInFHg1_sF0cAjWoDKSSV-PF4HSexLnLaPAx2bMj1ZGJHAf6JXICqVqRzxXkEsoWuirHxdJY5NSlQfkw4jFXl1CPrPI9doUlZ2X7hW_f6_Afu6M8g";
 
             Assert.That(jwt, Is.EqualTo(expectedJwt));
         }
